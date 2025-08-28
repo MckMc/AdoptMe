6dc6e7cbe2c823bfa26a85e70b41b880674d7465
Manuel Cecchin <manuelcecchinkuhn@gmail.com}>
2025-08-28 19:42:59 -0300

    nuevp intento de vercel

--- api/index.js
+++ api/index.js
@@ -2,6 +2,16 @@ import serverless from 'serverless-http';
 import app from '../src/app.js';
 import { connectMongo } from '../src/db/mongo.js';
 
-await connectMongo();
+let mongoReady;
 
-export default serverless(app);
+export default async function handler(req, res) {
+  if (!mongoReady) {
+    mongoReady = connectMongo().catch(err => {
+      console.error('Mongo connect failed', err);
+      mongoReady = null;
+    });
+  }
+  await mongoReady;
+  const wrapped = serverless(app);
+  return wrapped(req, res);
+}
